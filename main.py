99bb5d4d9d413ab6fdbe4a9e306f01cb48cda56a
Alex.Dai <re757575@gmail.com>
2015-12-02 12:10:41 +0800

    Handler request

--- main.py
+++ main.py
@@ -3,13 +3,29 @@
 
 import webapp2
 import os
-from google.appengine.ext.webapp import
+from google.appengine.ext.webapp import template
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+
+        # 取得參數 name 的值
+        name = self.request.get("name")
+
+        # 取得所有參數
+        self.request.GET
+
+        # 取得參數 name 的值，若有兩個 name 參數則顯示最後一個
+        self.request.GET['name']
+
+        # 取得所有名為 name 參數值
+        self.request.GET.getall('name')
+
+        # 取得所有參數
+        self.request.GET.items()
+
         # 這邊設定要傳入頁面的參數 name，值為 Seth
         template_values = {
-            'name': 'Seth'
+            'name': name
         }
 
         # 指定 index.html 作為輸出的頁面
